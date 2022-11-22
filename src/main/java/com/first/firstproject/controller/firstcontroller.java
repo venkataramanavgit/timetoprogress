afbe4d1447b267c8eeea36cb1b39f955278ad9c8
VENKATA RAMANA MADEPALLI <190030966@kluniversity.in>
2022-11-22 21:16:39 +0530

    updated api for car

--- src/main/java/com/first/firstproject/controller/firstcontroller.java
+++ src/main/java/com/first/firstproject/controller/firstcontroller.java
@@ -1,15 +1,58 @@
 package com.first.firstproject.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.first.firstproject.entities.Car;
+import com.first.firstproject.repos.CarRepository;
+
+
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/car")
 public class firstcontroller {
-	
-//	
-//	    @RequestMapping("/")
-//	    public String index() {
-//	        return "index";
-//	    }
+
+    @Autowired
+    private CarRepository Carrep;
+    
+   
+    @GetMapping
+    public List<Car>   findAllCars() {
+        // Implement
+    	return  (List<Car>) Carrep.findAll();
+   
+    }
+ 
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> findCarrById(@PathVariable(value = "id") long id) {
+       // Implement
+    
+    	 Optional<Car> user =  Carrep.findById(id);
+    	 
+    	    if(user.isPresent()) {
+    	        return ResponseEntity.ok().body(user.get());
+    	    } else {
+    	        return ResponseEntity.notFound().build();
+    	    }
+    	
+    }
+ 
+    @PostMapping
+    public Car saveUser(@Validated @RequestBody Car car) {
+        return Carrep.save(car);
+    	
+    }
 	
 }
---
+++ src/main/java/com/first/firstproject/repos/Carservice.java
@@ -0,0 +1,23 @@
+package com.first.firstproject.repos;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.first.firstproject.entities.Car;
+
+
+
+@Service
+public class Carservice {
+
+
+}
